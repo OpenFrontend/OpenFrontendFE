49ce9f067139c9424923011cd7a377ac6e1ffb3a
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2022-10-24 17:49:48 +0530

    login and register modal implemented

--- src/Modules/Card/Card.jsx
+++ src/Modules/Card/Card.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal/Modal";
 import "./style.css";
 
 const Card = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="border rounded-lg img-wrap ">
-      <img src="https://source.unsplash.com/random" className="center" />
-    </div>
+    <>
+      {isOpen && <Modal toggleModal={toggleModal} />}
+      <a
+        onClick={toggleModal}
+        className="hover:cursor-pointer border rounded-lg img-wrap "
+      >
+        <img src="https://source.unsplash.com/random" className="center" />
+      </a>
+    </>
   );
 };
 
--- src/Modules/Modal/Modal.jsx
+++ src/Modules/Modal/Modal.jsx
@@ -1,9 +1,29 @@
- import React from 'react'
- 
- const Modal = () => {
-   return (
-     <div>Modal</div>
-   )
- }
- 
- export default Modal
+import React from "react";
+import styles from "../Modal/Modal.module.css";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+const Modal = (props) => {
+  return (
+    <>
+      <div className="sticky  rounded-sm w-full z-50 bg-white top-0">
+        <div className={`${styles.darkBG}`} />
+        <div className={styles.centered}>
+          <div className={`${styles.modal}`}>
+            <button className={styles.closeBtn} onClick={props.toggleModal}>
+              <XMarkIcon
+                onClick={props.toggleModal}
+                className="h-8 w-8 mx-1 cursor-pointer"
+                aria-hidden="true"
+              />
+            </button>
+            <div
+              className={`${styles.modalContent} items-center h-full flex justify-start`}
+            ></div>
+          </div>
+        </div>{" "}
+      </div>
+    </>
+  );
+};
+
+export default Modal;

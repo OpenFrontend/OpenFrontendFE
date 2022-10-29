4ddd12a9bef76a70cd05b0bfeb6659ed9178f873
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2022-10-29 18:09:17 +0530

    api seprate page done + redux toolkit implemented

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,13 +1,61 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { loginAPI, signupAPI } from "../services/user";
+
+export const loginItem = createAsyncThunk(
+  "user/loginItem",
+  async (payload, thunkAPI) => {
+    let response = await loginAPI(payload);
+    console.log("res=", response);
+    if (response.isSuccessful === true) {
+      // all localstorage work goes here
+      return response.data;
+    } else thunkAPI.rejectWithValue("network call failed");
+  }
+);
+export const signupItem = createAsyncThunk(
+  "user/signupItem",
+  async (payload, thunkAPI) => {
+    let response = await signupAPI(payload);
+    console.log("res=", response);
+    if (response.isSuccessful === true) {
+      return response.data;
+    } else thunkAPI.rejectWithValue("network call failed");
+  }
+);
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: { value: { name: "tarun", email: "", password: "" } },
+  initialState: { userInfo: [], loggedIn: false },
   reducers: {
-    login: (state, action) => {
-      state.value = action.payload;
+    logoutFunction: (state, action) => {
+      state.userInfo = "";
+      let updateConnector = false;
+      state.loggedIn = updateConnector;
+      // localStorage.removeItem("userdata");
+    },
+  },
+  extraReducers: {
+    [loginItem.fulfilled]: (state, action) => {
+      if (action.payload.token) {
+        state.userInfo = action.payload;
+        let updateConnector = true;
+        state.loggedIn = updateConnector;
+        localStorage.setItem("loggedIn", state.loggedIn);
+      } else {
+        state.loggedIn = false;
+      }
+    },
+    [signupItem.fulfilled]: (state, action) => {
+      if (action.payload.token) {
+        state.userInfo = action.payload;
+        let updateConnector = true;
+        state.loggedIn = updateConnector;
+        localStorage.setItem("loggedIn", state.loggedIn);
+      } else {
+        state.loggedIn = false;
+      }
     },
   },
 });
-
+export const { logoutFunction } = userSlice.actions;
 export default userSlice.reducer;
